56589f0146db27253231b82a359c545ebb0b3607
Herbert Tamayo <hftamayo@gmail.com>
2024-04-11 14:02:36 -0600

    rolling back the entrypoint file

--- jenkins/kubectl/docker/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-DOCKER_SOCKET=/var/run/docker.sock
-DOCKER_GROUP=docker
-DOCKER_GID=$(stat -c '%g' $DOCKER_SOCKET)
-groupadd -for -g $DOCKER_GID $DOCKER_GROUP
-usermod -aG $DOCKER_GROUP jenkins
-exec "$@"
