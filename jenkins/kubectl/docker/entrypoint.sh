1e2876bc2ed799560389bde541476450e383205a
Herbert Tamayo <hftamayo@gmail.com>
2024-04-30 15:35:14 -0600

    minor update on script and dockerversion

--- jenkins/kubectl/docker/entrypoint.sh
+++ jenkins/kubectl/docker/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Change the ownership of the Docker socket
-su -c "chown root:docker /var/run/docker.sock" root
+exec su -c "chown root:docker /var/run/docker.sock" root
 
 # Run the original Jenkins entrypoint script
 exec su -c "/sbin/tini -- /usr/local/bin/jenkins.sh" root
