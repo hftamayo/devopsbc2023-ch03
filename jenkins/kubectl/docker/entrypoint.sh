7203c65931a6cb9b9374ed30ff60e748d3ce6819
Herbert Tamayo <hftamayo@gmail.com>
2024-05-02 14:03:44 -0600

    trying a new approach to run commands taht require privileges

--- jenkins/kubectl/docker/entrypoint.sh
+++ jenkins/kubectl/docker/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Change the ownership of the Docker socket
-sudo chown root:docker /var/run/docker.sock
+chown root:docker /var/run/docker.sock
 
 # Run the original Jenkins entrypoint script
-exec sudo /sbin/tini -- /usr/local/bin/jenkins.sh
+exec sbin/tini -- /usr/local/bin/jenkins.sh
