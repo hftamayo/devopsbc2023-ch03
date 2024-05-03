825f9a99c9b0b5c65b2fa75152712d0a0bbe1af9
Herbert Tamayo <hftamayo@gmail.com>
2024-05-03 12:58:32 -0600

    adding extra safe verification to docker sock ownership

--- jenkins/kubectl/docker/entrypoint.sh
+++ jenkins/kubectl/docker/entrypoint.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+# If the docker group does not exist, create it.
+if ! getent group docker; then
+  groupadd -g 994 docker
+fi
+
+# Add the jenkins user to the docker group.
+usermod -aG docker jenkins
+
 # Change the ownership of the Docker socket
 chown root:docker /var/run/docker.sock
+
+# Check if the chown command was successful
+if [ "$(stat -c '%G' /var/run/docker.sock)" != "docker" ]; then
+  echo "Failed to change the ownership of the Docker socket"
+  exit 1
+fi
 
 # Run the original Jenkins entrypoint script
 exec tini -- /usr/local/bin/jenkins.sh
